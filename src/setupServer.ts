e9b4c6bad5c52a9fed1cc1c3f35a1a656f202131
Nabeel Ahmad <nab.ahmad55@gmail.com>
2025-10-13 10:00:35 +0500

    chat_project setup for routes socketIO Redis is complete

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -8,6 +8,11 @@ import cookieSession from 'cookie-session';
 import HTTP_STATUS from 'http-status-codes';
 import 'express-async-errors';
 import { config } from './config';
+import { Server } from 'socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
+import applcationRoutes from './routes'
+import { CustomError, IErrorResponce } from "./shared/globals/helpers/error-handler";
 
 const SERVER_PORT = 5000;
 
@@ -58,28 +63,59 @@ export class ChattyServer {
 
     }
 
-    private routeMiddleware(app: Application): void {}
+    private routeMiddleware(app: Application): void {
+        applcationRoutes(app);
+    }
 
-    private globalErrorHandler(app: Application): void {}
+    private globalErrorHandler(app: Application): void {
+        app.all('*', (req: Request, res: Response) => {
+            res.status(HTTP_STATUS.NOT_FOUND).json({ message: `${req.originalUrl} not found`})
+        });
+
+        app.use((error: IErrorResponce, _req: Request, res: Response, next: NextFunction) => {
+            console.log(error);
+            if(error instanceof CustomError){
+                return res.status(error.statusCode).json(error.serializeErrors); 
+            }
+            next();
+        });
+    }
 
     private async startServer(app: Application): Promise<void> {
         try{
             const httpServer: http.Server = new http.Server(app);
-            //this.createSocketIO(httpServer);
+            const socketIO: Server = await this.createSocketIO(httpServer);
             this.startHttpServer(httpServer);
+            this.socketIOConnection(socketIO);
         }catch(error){
             console.log(error);
         }
     }
 
-    private createSocketIO(httpServer: http.Server): void {}
+    private async createSocketIO(httpServer: http.Server): Promise<Server> {
+        const io: Server = new Server(httpServer, {
+            cors: {
+                origin: config.CLIENT_URL,
+                methods: ['GET', 'POST' , 'PUT', 'DELETE', 'OPTIONS']
+            }
+        });
+        const pubClient = createClient({url: config.REDIS_HOST });
+        const subClient = pubClient.duplicate();
+        await Promise.all([pubClient.connect(), subClient.connect()]);
+        io.adapter(createAdapter(pubClient,subClient));
+
+        return io;
+
+    }
 
     private startHttpServer(httpServer: http.Server): void {
-
+        console.log(`Server Has Started with process ${process.pid}`);
         httpServer.listen(SERVER_PORT, () => {
             console.log(`Server running on port ${SERVER_PORT}`);
         });
     }
+
+    private socketIOConnection(io: Server): void {}
 
 
 
