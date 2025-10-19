af0ffc3cb5ef5dc29a455d1522c9b06ed009fce3
Nabeel Ahmad <nab.ahmad55@gmail.com>
2025-10-19 19:06:38 +0500

    feature/auth-feature complete 40 lecture

--- src/shared/globals/decorators/joi-validation.decorators.ts
+++ src/shared/globals/decorators/joi-validation.decorators.ts
@@ -6,6 +6,16 @@ type IJoiDecorator = (target: any, key: string, descriptor:PropertyDescriptor) =
 
 export function joiValidation(schema: ObjectSchema): IJoiDecorator {
   return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+    descriptor.value = async function(...args: any[]){
+      const req: Request = args[0];
+      const {error} = await Promise.resolve(schema.validate(req.body));
+      if(error?.details){
+        throw new JoiRequestValidationError(error.details[0].message)
+      }
+      return originalMethod.apply(this,args);
+    };
+    return descriptor;
 
   }
 }
